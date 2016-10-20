932cf85cf4f9cea0c2fc0397b33e759e76d92717
Justin Karneges <justin@karneges.com>
2016-10-20 11:59:17 -0700

    update copyright year

--- src/qzmqvalve.cpp
+++ src/qzmqvalve.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Justin Karneges
+ * Copyright (C) 2012-2016 Justin Karneges
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
